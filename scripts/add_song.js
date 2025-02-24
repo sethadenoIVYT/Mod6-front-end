42fd9c2cbda493d40445246fb76fb8bfa0159f01
sethadenoIVYT <156937009+sethadenoIVYT@users.noreply.github.com>
2025-02-23 20:45:48 -0500

    Update add_song.js

--- scripts/add_song.js
+++ scripts/add_song.js
@@ -14,7 +14,7 @@ async function addSong() {
         genre: document.querySelector("#genre").value ? document.querySelector("#genre").value.split(",") : []
     }
 
-    const response = await fetch("https://exclusive-temporal-screwdriver.glitch.me/api/songs", {
+    const response = await fetch("https://cut-ajar-rate.glitch.me/api/songs", {
         method: "POST",
         headers:{
             "Content-Type" : "application/json"
